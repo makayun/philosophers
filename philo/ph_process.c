d6e643600d70e25ce5df0167dd7e74cc3c654110
Maksim Makagonov <maxmakagonov@Maksims-MacBook-Pro.local>
2024-01-24 20:22:30 +0100

    hope it'll work better in the campus

--- philo/ph_process.c
+++ philo/ph_process.c
@@ -6,7 +6,7 @@
 /*   By: maxmakagonov <maxmakagonov@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:28:54 by mmakagon          #+#    #+#             */
-/*   Updated: 2024/01/24 20:19:54 by maxmakagono      ###   ########.fr       */
+/*   Updated: 2024/01/24 14:16:12 by maxmakagono      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,28 @@
 
 void ph_die(long mcsec_current, t_philosopher *philo)
 {
-	pthread_mutex_lock(&philo->common_data->state_change);
+	pthread_mutex_lock(&philo->common_data->state_check);
 	if (philo->common_data->someone_died == false)
 	{
 		philo->common_data->someone_died = true;
 		printf ("%ld %d %s", (mcsec_current - philo->rules.mcsec_start) / 1000, philo->id, STR_DEAD);
 	}
-	pthread_mutex_unlock(&philo->common_data->state_change);
+	pthread_mutex_unlock(&philo->common_data->state_check);
 }
 
 int	ph_state_change(t_philosopher *philo, int new_state)
 {
 	if (ph_get_current_mcsec(&philo->mcsec_current) - philo->last_meal >= philo->rules.mcsec_to_die)
 		return (ph_die(philo->mcsec_current, philo), STOP);
+	pthread_mutex_lock(&philo->common_data->state_check);
+	if (new_state == ATE_ENOUGH)
+		philo->common_data->philos_ate_enough++;
+	else if (philo->common_data->someone_died == true
+			|| philo->common_data->philos_ate_enough >= philo->rules.philos_total)
+		return (pthread_mutex_unlock(&philo->common_data->state_check), STOP);
+	pthread_mutex_unlock(&philo->common_data->state_check);	
 	pthread_mutex_lock(&philo->common_data->state_change);
-	if (philo->common_data->philos_ate_enough >= philo->rules.philos_total
-		|| philo->common_data->someone_died == true)
-		new_state = STOP;
-	else if (new_state == TAKING_FORK)
+	if (new_state == TAKING_FORK)
 		ph_print_message(philo, STR_FORK);
 	else if (new_state == EATING)
 		ph_print_message(philo, STR_EATING);
@@ -39,9 +43,6 @@ int	ph_state_change(t_philosopher *philo, int new_state)
 		ph_print_message(philo, STR_SLEEPING);
 	else if (new_state == THINKING)
 		ph_print_message(philo, STR_THINKING);
-	else if (new_state == ATE_ENOUGH)
-		if (++philo->common_data->philos_ate_enough >= philo->rules.philos_total)
-			new_state = STOP;
 	pthread_mutex_unlock(&philo->common_data->state_change);
 	return (new_state);
 }
@@ -69,15 +70,13 @@ void	*ph_process(void *arg)
 	long			offset;
 
 	philo = (t_philosopher *)arg;
-	offset = (philo->rules.philos_total - philo->id) * philo->rules.philos_total;
-	// ph_wait_until(philo, philo->rules.mcsec_start + offset);
+	offset = (philo->rules.philos_total - philo->id) * philo->rules.philos_total * 2;
+	ph_wait_until(philo, philo->rules.mcsec_start + offset);
 	while (ph_state_change(philo, THINKING) != STOP)
 	{
-		ph_wait_until(philo, philo->last_meal + offset);
-
-		if (philo->id % 2 == 0)
+		if (philo->id % 2 != 0)
 		{
-			ph_wait_until(philo, philo->mcsec_current + philo->rules.philos_total * 50);
+			ph_wait_until(philo, philo->mcsec_current + philo->rules.philos_total * 200);
     		if (ph_eat(philo, philo->left_fork, philo->right_fork) == STOP)
 				return (NULL);
 		}
