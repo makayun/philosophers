6472b39fcdf472883129a5b3a14dfe2ef3d6a504
makayun <115617658+makayun@users.noreply.github.com>
2024-04-22 15:20:40 +0200

    Delete philo/tests directory

--- philo/tests/test4.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#define CONVERT(A) A / 1000
-
-int main()
-{
-	printf ("%d", CONVERT(2000));
-}
--- philo/tests/test8.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#define STR_ONE "is dead"
-
-int main()
-{
-	printf("%s", STR_ONE);
-}
